c94226181c09192ab2582ab8bbb959d75e206189
Killbot6 <43080263+devinSchminke@users.noreply.github.com>
2022-06-06 09:02:20 -0500

    Update follina_workaround_automation.py

--- follina_workaround_automation.py
+++ follina_workaround_automation.py
@@ -2,21 +2,10 @@ import os
 import keyboard
 import time
 
-#This program is free software: you can redistribute it and/or modify
-#it under the terms of the GNU General Public License as published by
-#the Free Software Foundation, either version 3 of the License, or
-#(at your option) any later version.
+#This is software has the MIT License.
 
-#This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#this is going to need to ran as admin probably. 
 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-# Also add information on how to contact you by electronic and paper mail.
-
-#this is going need to ran as admin probably. 
 os.system(
     'cd C:\ ' + #this will navigate us to the C drive, and how we are there in the prompt
     '& mkdir FollinaRegBackup' + #this will create a folder on the C/ drive to save the backup in.
